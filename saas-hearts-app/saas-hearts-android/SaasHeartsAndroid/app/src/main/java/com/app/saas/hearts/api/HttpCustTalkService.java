168d0dc255d7ed44b80535b625942e2f8ba3c3be
gaoqiang <1105107264@qq.com>
2023-01-05 13:37:10 +0800

    api创建完成

---
+++ saas-hearts-app/saas-hearts-android/SaasHeartsAndroid/app/src/main/java/com/app/saas/hearts/api/HttpCustTalkService.java
@@ -0,0 +1,34 @@
+package com.app.saas.hearts.api;
+
+import com.app.saas.hearts.entity.CustTalk;
+import com.app.saas.hearts.entity.PageInfo;
+
+import io.reactivex.Observable;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Copyright (C), 2019-2023, 佛生
+ * FileName: HttpCustTalkService
+ * Author: 佛学徒
+ * Date: 2023/1/4 17:26
+ * Description: app话题api
+ * History:
+ */
+public interface HttpCustTalkService {
+
+    //新增
+    @POST("/saas-hearts-service/api/custTalk/addTalk")
+    Observable<Object> addTalk(@Body CustTalk custTalk);
+
+    //列表
+    @GET("/saas-hearts-service/api/custTalk/getTalkList")
+    Observable<PageInfo<CustTalk>> getTalkList(@Query("pageIndex") int pageIndex, @Query("pageSize") int pageSize, @Query("status") int status);
+
+    //详情
+    @GET("/saas-hearts-service/api/custTalk/getTalkDetail")
+    Observable<CustTalk> getTalkDetail(@Query("talkId") int talkId);
+
+}
--- saas-hearts-app/saas-hearts-android/SaasHeartsAndroid/app/src/main/java/com/app/saas/hearts/ui/home/HomeFragment.kt
+++ saas-hearts-app/saas-hearts-android/SaasHeartsAndroid/app/src/main/java/com/app/saas/hearts/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.app.saas.hearts.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +10,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.app.saas.hearts.R
+import com.app.saas.hearts.api.HttpManage
 import com.app.saas.hearts.databinding.FragmentHomeBinding
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class HomeFragment : Fragment() {
 
@@ -35,11 +40,37 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        getData()
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun getData(){
+        HttpManage.getHttpCustUserService().getList(0,0,0)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : io.reactivex.Observer<Any> {
+                override fun onSubscribe(d: Disposable) {
+                    Log.d("TAG", "onSubscribe")
+                }
+
+                override fun onNext(result: Any) {
+                    Log.d("TAG", "请求成功:$result")
+
+                }
+
+                override fun onError(e: Throwable) {
+
+                    Log.e("TAG", "请求失败" + e.message)
+                }
+
+                override fun onComplete() {
+                    Log.d("TAG", "请求完成")
+                }
+            })
     }
 }
